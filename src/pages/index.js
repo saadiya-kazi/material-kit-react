0378bae405991b0fad6f76c4e00075ae28b84cbc
saadiyakazi <saadiya@optimizeapp.com>
2022-02-12 11:59:54 +0300

    add screen for list of coach and class

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,21 @@
-import Head from 'next/head';
-import { Box, Container, Grid } from '@mui/material';
-import { Budget } from '../components/dashboard/budget';
-import { LatestOrders } from '../components/dashboard/latest-orders';
-import { LatestProducts } from '../components/dashboard/latest-products';
-import { Sales } from '../components/dashboard/sales';
-import { TasksProgress } from '../components/dashboard/tasks-progress';
-import { TotalCustomers } from '../components/dashboard/total-customers';
-import { TotalProfit } from '../components/dashboard/total-profit';
-import { TrafficByDevice } from '../components/dashboard/traffic-by-device';
-import { DashboardLayout } from '../components/dashboard-layout';
+import Head from "next/head";
+import { Box, Container, Grid } from "@mui/material";
+import { Budget } from "../components/dashboard/budget";
+import { LatestOrders } from "../components/dashboard/latest-orders";
+import { LatestProducts } from "../components/dashboard/latest-products";
+import { Sales } from "../components/dashboard/sales";
+import { TasksProgress } from "../components/dashboard/tasks-progress";
+import { TotalCustomers } from "../components/dashboard/total-customers";
+import { TotalProfit } from "../components/dashboard/total-profit";
+import { TrafficByDevice } from "../components/dashboard/traffic-by-device";
+import { DashboardLayout } from "../components/dashboard-layout";
 
 const Dashboard = () => (
   <>
     <Head>
-      <title>
-        Dashboard | Material Kit
-      </title>
+      <title>Dashboard | FlexGym</title>
     </Head>
-    <Box
+    {/* <Box
       component="main"
       sx={{
         flexGrow: 1,
@@ -103,14 +101,10 @@ const Dashboard = () => (
           </Grid>
         </Grid>
       </Container>
-    </Box>
+    </Box> */}
   </>
 );
 
-Dashboard.getLayout = (page) => (
-  <DashboardLayout>
-    {page}
-  </DashboardLayout>
-);
+Dashboard.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
 export default Dashboard;
